49f4cc3acc9ffb92828cf8b8dfb9e2d9e3378653
Austen DeWolf <austen.dewolf@smartsheet.com>
2017-03-08 10:34:37 -0800

    Initial Commit
    Open a pre-built template, copy that template, and replace values
    within it with data defined in code or from another source.

---
+++ DocumentGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using Novacode;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "\\Data\\" + DateTime.Now.ToString("MMMM", CultureInfo.InvariantCulture) + "_" + DateTime.Now.Year + "_UtilityBill.docx";
+
+            var newUtilityBillPath = CreateFileFromTemplate("\\Data\\Test_Template.docx", fileName);
+
+            var utilityBill = GetTemplate(newUtilityBillPath);
+
+            utilityBill.ReplaceText("{{current_date}}", DateTime.Now.ToLongDateString());
+            utilityBill.ReplaceText("{{user_name}}", "Austen");
+            utilityBill.ReplaceText("{{boss_name}}", "Boss Austen");
+
+            utilityBill.Save();
+        }
+
+        static DocX GetTemplate(string filePath)
+        {
+            try
+            {
+                DocX docx = DocX.Load(filePath);
+
+                return docx;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        static string CreateFileFromTemplate(string templatePath, string fileName)
+        {
+            try
+            {
+                var sourceFilePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + templatePath;
+                var destinationFilePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + fileName;
+
+                File.Copy(sourceFilePath, destinationFilePath, true);
+
+                return destinationFilePath;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
